a51133cb0704a52856641c77904a3fe711714433
diasqazaqbro <diasqazaqbro@mail.ru>
2024-02-11 01:11:40 +0600

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,11 @@
 
 
 export default function handler(req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "*"); 
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, PUT, POST, DELETE, OPTIONS"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	res.status(200).json({ name: 'Dias' })
 }
